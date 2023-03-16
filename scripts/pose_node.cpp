ce703e443d2458c5973c5fed27613fad970480c4
dschweickard <Dirk.Schweickard@stud.tu-darmstadt.de>
2023-03-16 08:17:30 +0000

    Updated service

--- scripts/pose_node.cpp
+++ scripts/pose_node.cpp
@@ -3,43 +3,44 @@
 #include <TiagoBears_PoseEstimation/PoseEstimation.h>
 
 
- tf2::Matrix3x3 CorrectRotMatrix(tf2::Matrix3x3 &matrix){
-   // conver them to Eigen matrix
-   Eigen::MatrixXf eigenMatrix = Eigen::MatrixXf::Zero(3,3);
-   for (int i=0; i<3; i++){
-     for (int j=0; j<3; j++){
-       eigenMatrix(i,j) = matrix[i][j];
-     }
-   }
-   for (int i=0;i<3; i++){
-     // take a sub matrix with size (3-i)x(3-i)
-     Eigen::MatrixXf subMatrix = eigenMatrix.block(0,0,3-i,3-i);
-     // update the submatrix to have absolute values
-     subMatrix = subMatrix.cwiseAbs();
-     // find the max element in the sub matrix
-     int max_x, max_y;
-     float maxVal = subMatrix.maxCoeff(&max_x, &max_y);
-     // swap the rows and columns such that the max value is in (3-i), (3-i) position
-     eigenMatrix.row(2-i).swap(eigenMatrix.row(max_x));
-     eigenMatrix.col(2-i).swap(eigenMatrix.col(max_y));
-     // multipy the row with -1 if the max value is negative
-     if (eigenMatrix(2-i,2-i) < 0){
-       eigenMatrix.row(2-i) = -1 * eigenMatrix.row(2-i);
-     }
-   }
-   // convert back to tf2::Matrix3x3
-   for (int i=0; i<3; i++){
-     for (int j=0; j<3; j++){
-       matrix[i][j] = eigenMatrix(i,j);
-     }
-   }
-   return matrix;
- }
+
+tf2::Matrix3x3 CorrectRotMatrix(tf2::Matrix3x3 &matrix){
+  // conver them to Eigen matrix
+  Eigen::MatrixXf eigenMatrix = Eigen::MatrixXf::Zero(3,3);
+  for (int i=0; i<3; i++){
+    for (int j=0; j<3; j++){
+      eigenMatrix(i,j) = matrix[i][j];
+    }
+  }
+  for (int i=0;i<3; i++){
+    // take a sub matrix with size (3-i)x(3-i)
+    Eigen::MatrixXf subMatrix = eigenMatrix.block(0,0,3-i,3-i);
+    // update the submatrix to have absolute values
+    subMatrix = subMatrix.cwiseAbs();
+    // find the max element in the sub matrix
+    int max_x, max_y;
+    float maxVal = subMatrix.maxCoeff(&max_x, &max_y);
+    // swap the rows and columns such that the max value is in (3-i), (3-i) position
+    eigenMatrix.row(2-i).swap(eigenMatrix.row(max_x));
+    eigenMatrix.col(2-i).swap(eigenMatrix.col(max_y));
+    // multipy the row with -1 if the max value is negative
+    if (eigenMatrix(2-i,2-i) < 0){
+      eigenMatrix.row(2-i) = -1 * eigenMatrix.row(2-i);
+    }
+  }
+  // convert back to tf2::Matrix3x3
+  for (int i=0; i<3; i++){
+    for (int j=0; j<3; j++){
+      matrix[i][j] = eigenMatrix(i,j);
+    }
+  }
+  return matrix;
+}
 
 
 bool service_callback(TiagoBears_PoseEstimation::PoseEstimation::Request &req, TiagoBears_PoseEstimation::PoseEstimation::Response &res)
 {
-  float leaf_size = 0.003f;
+  float leaf_size = 0.0025f;
 
   float crop_x_min = -0.6f;
   float crop_x_max = +0.6f;
@@ -47,10 +48,15 @@ bool service_callback(TiagoBears_PoseEstimation::PoseEstimation::Request &req, T
   float crop_y_max = +1.1f;
   float crop_z_min = -1.0f;
   float crop_z_max = +1.0f;
-  float plane_seg_dist_treshold = 0.005f;
-  float cluster_tolerance = 0.004f;
+  float plane_seg_dist_treshold = 0.0065f;
+  
+  int MeanK = 50;
+  float StddevMulThresh = 1.0f;
+
+  float cluster_tolerance = 0.0035f;
   int cluster_size_min = 150;
-  int cluster_size_max = 1500;
+  int cluster_size_max = 1100;
+
 
 
   sensor_msgs::PointCloud2 msg_cloud;
@@ -62,30 +68,30 @@ bool service_callback(TiagoBears_PoseEstimation::PoseEstimation::Request &req, T
         msg_cloud = *msg;
   ROS_INFO_STREAM("FrameID  " << msg->header.frame_id);
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr model_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr model_cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
   pcl::PCLPointCloud2 cloud_blob;
-  pcl::io::loadPCDFile ("cube.pcd", cloud_blob);
+  pcl::io::loadPCDFile ("src/TiagoBears_PoseEstimation/models/cube.pcd", cloud_blob);
   pcl::fromPCLPointCloud2 (cloud_blob, *model_cloud); //* convert from pcl/PCLPointCloud2 to pcl::PointCloud<T>
   
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr input_filtered_cloud(new pcl::PointCloud< pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr msg_cloud_pcl(new pcl::PointCloud< pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filterd_vox(new pcl::PointCloud< pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_filtered_cloud(new pcl::PointCloud< pcl::PointXYZRGB>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr msg_cloud_pcl(new pcl::PointCloud< pcl::PointXYZRGB>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filterd_vox(new pcl::PointCloud< pcl::PointXYZRGB>);
 
   // Convert from PointCloud2 to PointCloud
   //pcl::fromPCLPointCloud2 (msg_cloud, *msg_cloud_pcl);
   pcl::fromROSMsg(msg_cloud, *msg_cloud_pcl);
 
-  pcl::VoxelGrid<pcl::PointXYZ> vox;
+  pcl::VoxelGrid<pcl::PointXYZRGB> vox;
   vox.setInputCloud (msg_cloud_pcl);
   vox.setLeafSize (leaf_size,leaf_size,leaf_size);
   vox.filter (*cloud_filterd_vox);
 
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cropped(new pcl::PointCloud< pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cropped(new pcl::PointCloud< pcl::PointXYZRGB>);
 
   // Set up CropBox and filter input PointCloud
-  pcl::CropBox<pcl::PointXYZ> crop;
+  pcl::CropBox<pcl::PointXYZRGB> crop;
   crop.setInputCloud (cloud_filterd_vox);
   // y and z parameter seem to crop same dimension just inverted?
   crop.setMin(Eigen::Vector4f(crop_x_min, crop_y_min, crop_z_min, 0.));
@@ -94,11 +100,11 @@ bool service_callback(TiagoBears_PoseEstimation::PoseEstimation::Request &req, T
   ROS_INFO("[filtered]");
 
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_seg (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_seg (new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZRGB>);
 
   // Create the segmentation object for the planar model and set all the parameters
-  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  pcl::SACSegmentation<pcl::PointXYZRGB> seg;
   pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
   pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
   seg.setOptimizeCoefficients (true);
@@ -116,7 +122,7 @@ bool service_callback(TiagoBears_PoseEstimation::PoseEstimation::Request &req, T
 
 
   // Extract planar inliers from input cloud
-  pcl::ExtractIndices<pcl::PointXYZ> extract;
+  pcl::ExtractIndices<pcl::PointXYZRGB> extract;
   extract.setInputCloud (cloud_cropped);
   extract.setIndices(inliers);
   extract.setNegative (false);
@@ -129,14 +135,22 @@ bool service_callback(TiagoBears_PoseEstimation::PoseEstimation::Request &req, T
   extract.filter (*cloud_seg);
   
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cubes (new pcl::PointCloud<pcl::PointXYZ>(*cloud_seg));
 
-  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cubes (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+  pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor;
+  sor.setInputCloud (cloud_seg);
+  sor.setMeanK (MeanK);
+  sor.setStddevMulThresh (StddevMulThresh);
+  sor.filter (*cloud_cubes);
+  
+
+  pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
 
   tree->setInputCloud (cloud_cubes);
 
   std::vector<pcl::PointIndices> cluster_indices;
-  pcl::EuclideanClusterExtraction<pcl::PointXYZ> ece;
+  pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ece;
   ece.setClusterTolerance (cluster_tolerance); //cluster_tolerance 
   ece.setMinClusterSize (cluster_size_min); //cluster_min_size
   ece.setMaxClusterSize (cluster_size_max); //cluster_max_size
@@ -147,13 +161,13 @@ bool service_callback(TiagoBears_PoseEstimation::PoseEstimation::Request &req, T
   
 
   std::vector<sensor_msgs::PointCloud2::Ptr> pc2_clusters;
-  std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clusters_vec;
+  std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clusters_vec;
 
-  pcl::ExtractIndices<pcl::PointXYZ> extract_cluster;
+  pcl::ExtractIndices<pcl::PointXYZRGB> extract_cluster;
   
   for (auto& cluster_idx : cluster_indices)
   {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZRGB>);
     pcl::PointIndices::Ptr idx (new pcl::PointIndices);
     *idx = cluster_idx;
     extract_cluster.setInputCloud(cloud_cubes);
@@ -171,9 +185,9 @@ bool service_callback(TiagoBears_PoseEstimation::PoseEstimation::Request &req, T
   //ICP
   std::vector<double> score_vec(28, 0.0);
   //std::vector<double> score_vec;
-  std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr > icp_vec;
+  std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr > icp_vec;
 
-  pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+  pcl::IterativeClosestPoint<pcl::PointXYZRGB, pcl::PointXYZRGB> icp;
   icp.setMaximumIterations (10000);
   icp.setTransformationEpsilon (1e-9);
   //icp.setMaxCorrespondenceDistance (0.003);
@@ -187,7 +201,7 @@ bool service_callback(TiagoBears_PoseEstimation::PoseEstimation::Request &req, T
     icp.setInputSource (model_cloud);
     icp.setInputTarget (clouds);
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_icp(new pcl::PointCloud< pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_icp(new pcl::PointCloud< pcl::PointXYZRGB>);
 
     icp.align (*cloud_icp);
     icp_vec.push_back(cloud_icp);
